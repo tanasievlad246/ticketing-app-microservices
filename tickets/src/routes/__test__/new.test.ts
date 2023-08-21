f4e3e7dc9489c488f1e23d3ac4efa451245e11b3
Tanasie Vlad <tanasie.vlad@dandsltd.com>
2023-08-21 17:13:23 +0300

    added test for mock nats wrapper

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/Ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
     const response = await request(app)
@@ -69,4 +70,19 @@ it('creates a ticket with valid inputs', async () => {
     expect(tickets.length).toEqual(1);
     expect(tickets[0].price).toEqual(11);
     expect(tickets[0].title).toEqual('Test title');
+});
+
+it('publishes an event', async () => {
+    const title = 'Test title';
+    const price = 11;
+
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title,
+            price
+        }).expect(201);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
