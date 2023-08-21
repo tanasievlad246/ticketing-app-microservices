5f195d191f720d8a619db7f7d42f4bc0dc1a21f9
Tanasie Vlad <tanasie.vlad@dandsltd.com>
2023-08-21 17:24:59 +0300

    Moved nats values to env variables, replaced client id with pod name

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -9,12 +9,22 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
         throw new Error("MONGO_URI must be defined");
     }
+    if (!process.env.NATS_CLUSTER_ID) {
+        throw new Error("NATS_CLUSTER_ID env var must be defined");
+    }
+    if (!process.env.NATS_CLIENT_ID) {
+        throw new Error("NATS_CLIENT_ID env var must be defined");
+    }
+    if (!process.env.NATS_URL) {
+        throw new Error("NATS_URL env var must be defined");
+    }
+
     try {
         await mongoose.connect(process.env.MONGO_URI, {
             autoIndex: true,
         });
         console.log("Connected to MongoDB");
-        await natsWrapper.connect("ticketing", "tickets", "http://nats-srv:4222");
+        await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
         natsWrapper.client.on('close', () => {
             console.log('NATS connection closed!');
             process.exit();
